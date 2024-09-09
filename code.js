c0592d15fa907b84dad29897828e8abba6b146b2
Existism <126028748+Existism@users.noreply.github.com>
2024-09-08 21:16:32 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    // Login function
+    document.getElementById("startclicking").addEventListener("click", function() {
+        // Hides old elements
+        document.getElementById("startclicking").style.display = "none";
+        document.getElementById("welcome").style.display = "none";
+        
+        // Initialize clicks
+        let clicks = 0;
+        
+        // Create new elements
+        const click = document.createElement("h1");
+        const clickbutton = document.createElement("button");
+        
+        // Set text and IDs
+        clickbutton.innerText = "Click Me";
+        clickbutton.id = "clickbutton";
+        click.innerText = `Value: ${clicks}`;
+        
+        // Get the container where the new elements will go
+        const container = document.getElementById("container");
+        
+        // Append the new elements to the container
+        document.body.prepend(container);
+        document.body.prepend(click);
+        container.prepend(clickbutton);
+        
+        // Attach event listener to the dynamically created button
+        clickbutton.addEventListener("click", function() {
+            clicks++;
+            click.innerText = `Value: ${clicks}`;
+        });
+    });
+};
